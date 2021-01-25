51b36dc2cbb44efc34e059cdb503ff4a2c9f3d67
hahia <49113803+hahia@users.noreply.github.com>
2021-01-25 17:52:41 +0800

    Update 1_velocyto.sh

--- 1_velocyto.sh
+++ 1_velocyto.sh
@@ -1,5 +1,2 @@
 
-
-nohup velocyto run10x -m /cluster/huanglab/hhuang/project/Lulab/RNA_velocity/rmsk/mm10_rmsk.gtf
-
 nohup velocyto run -o ../RNA_velocity/loom/ -m ../RNA_velocity/rmsk/mm10_rmsk.gtf ./A10/outs/possorted_genome_bam.bam /cluster/database/10xgenomicDB/refdata-cellranger-mm10-3.0.0/genes/genes.gtf >../RNA_velocity/loom/1.log &
